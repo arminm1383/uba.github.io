4204673d6b750436b9fdf7d9f2e1ec0b23518740
armin-m1 <arminm1@uci.edu>
2026-02-02 14:34:11 -0800

    Implemented InfiniteCarousel on Home Page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,10 +10,10 @@ function Navbar () {
                         <img src = "/images/Home.png" className={"home-icon"} alt="Home Icon"/>
                     </a>
                 </li>
-                <li><a href={"/about"} className={"nav-item"}>About</a></li>
-                <li><a href={"/events"} className={"nav-item"}>Events</a></li>
-                <li><a href={"/board"} className={"nav-item"}>Board</a></li>
-                <li><a href={"/get-involved"} className={"nav-item"}>Get Involved</a></li>
+                <li><a href={"/about"} className={"nav-item"}>ABOUT</a></li>
+                <li><a href={"/events"} className={"nav-item"}>EVENTS</a></li>
+                <li><a href={"/board"} className={"nav-item"}>BOARD</a></li>
+                <li><a href={"/get-involved"} className={"nav-item"}>CONTACT</a></li>
             </ul>
         </nav>
     )
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,11 +2,13 @@ import "../styles/Home.css";
 import {useEffect} from "react";
 import paperFilter from "../assets/paper.png";
 import Navbar from "../components/Navbar";
+import { motion } from "motion/react";
+import InfiniteCarousel from "../components/InfiniteCarousel"
 
 function Home () {
 
     useEffect(() => {
-        document.body.style.backgroundColor = "#6788AF";
+        document.body.style.backgroundColor = "#F9F9F9";
         document.body.style.backgroundImage = `url(${paperFilter})`;
         document.body.style.backgroundBlendMode = "multiply";
         document.body.style.backgroundSize = "cover";
@@ -22,10 +24,21 @@ function Home () {
     return (
         <>
             <Navbar />
-            <div className="hero-section">
+            <motion.div className="hero-section"
+            initial = {{opacity: 0}}
+            animate = {{opacity: 80}}
+            transition = {{duration: 1}}>
                 <img src = "/images/UBA_logo.png" alt = "UBA Logo" className = "logo"/>
-                <h1 className="headerText">undergraduate business association</h1>
-            </div>
+                <motion.div className={"textContainer"}
+                initial = {{opacity: 0}}
+                animate = {{opacity: 100}}
+                transition = {{duration: 1}}>
+                    <h1 className="headerText">Undergraduate Business Association</h1>
+                    <h1 className="subText">Dedicated to helping students discover
+                        their career path & build strong professional skills</h1>
+                </motion.div>
+            </motion.div>
+            <InfiniteCarousel />
         </>
     )
 }
